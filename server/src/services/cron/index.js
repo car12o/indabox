581a393bd4239ca73d5071550382b356f041e773
car12o <car12o@users.noreply.github.com>
2020-05-07 23:01:53 +0100

    info log when cron is registered

--- server/src/services/cron/index.js
+++ server/src/services/cron/index.js
@@ -9,15 +9,20 @@ const jobs = [
 
 const init = () => {
   jobs.forEach((args) => {
+    const [cronTime] = args
     const cron = job(...args)
     cron.start()
     crons.push(cron)
+
+    log.info(`Cron job registered "${cronTime}"`)
   })
 }
 
 const stop = () => {
   crons.forEach((cron) => {
     cron.stop()
+
+    log.info("Cron job unregistered")
   })
 }
 
