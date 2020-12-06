3439f0741e52a785b4d58dd83eb61801088f9539
car12o <car12o@users.noreply.github.com>
2020-12-06 21:27:30 +0000

    chore: add timeout

--- server/src/services/password/index.js
+++ server/src/services/password/index.js
@@ -14,6 +14,7 @@ const genPassword = async () => {
     user.password = hashPassword(password)
     await user.save()
     await sendCreatedUserEmail({ user: { ...user.toJSON(), password } })
+    await new Promise((res) => setTimeout(res, 2500))
   }
 }
 
