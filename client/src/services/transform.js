e62e3ae5104c6b973063b717fd3f0df4c6017c0a
car12o <car12o@users.noreply.github.com>
2021-02-21 19:53:30 +0000

    fix: user pw change email template

--- client/src/services/transform.js
+++ client/src/services/transform.js
@@ -1,6 +1,6 @@
 import { compose, entries, map, join } from "lodash/fp"
 
-export const formatDate = (date) => new Date(date).toLocaleString("pt-PT")
+export const formatDate = (date) => date && new Date(date).toLocaleString("pt-PT")
 
 export const toQueryString = compose(
   (str) => (str ? `?${str}` : ""),
